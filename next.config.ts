6a2b618420ff42f9df6f3c91efdc734c76b16867
Lublu <amiribraev22002@gmail.com>
2025-11-05 10:56:09 +0000

    Error: Service account object must contain a string "project_id" propert

--- next.config.ts
+++ next.config.ts
@@ -30,9 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    FIREBASE_SERVICE_ACCOUNT_JSON: process.env.FIREBASE_SERVICE_ACCOUNT_JSON,
-  }
 };
 
 export default nextConfig;
